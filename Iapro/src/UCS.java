e11e41ddd4d6819fd5d07fc540244d1d0dc34664
FloryanKieffer <floryan@mbp-de-floryan.home>
2021-04-25 13:56:33 +0200

    UCS class

---
+++ Iapro/src/UCS.java
@@ -0,0 +1,163 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class UCS {
+	
+	private Puzzle racine;
+	private TreeSet<Puzzle> frontiere;
+	private static final String[] ACTIONS = { "H", "B", "G", "D" };
+	private Set<Puzzle> explored;
+	private Puzzle end;
+	
+	public UCS(Puzzle racine) {
+		this.racine = racine;
+		this.end = racine;
+		this.explored = new HashSet<Puzzle>();
+		this.frontiere = new TreeSet<Puzzle>();
+	}
+	
+	public int Score(Puzzle puzzle, String action) {
+		int x = 0;
+		int y = 0;
+		int score;
+		for (int i=0;i<puzzle.getN();i++) {
+			for(int j=0;j<puzzle.getN();j++) {
+				if(puzzle.getTab()[i][j]==-1) {
+					x=i;
+					y=j;
+				}
+					
+			}
+		}
+		if(action.equals("H")) {
+			if(puzzle.getTab()[x-1][y]%2==0) {
+				return puzzle.getScore()+1;
+			}
+			else {
+				return puzzle.getScore()+2;
+			}
+		}
+		else if(action.equals("B")) {
+			if(puzzle.getTab()[x+1][y]%2==0) {
+				return puzzle.getScore()+1;
+			}
+			else {
+				return puzzle.getScore()+2;
+			}
+		}
+		else if(action.equals("G")) {
+			if(puzzle.getTab()[x][y-1]%2==0) {
+				return puzzle.getScore()+1;
+			}
+			else {
+				return puzzle.getScore()+2;
+			}
+		}
+		else {
+			if(puzzle.getTab()[x][y+1]%2==0) {
+				return puzzle.getScore()+1;
+			}
+			else {
+				return puzzle.getScore()+2;
+			}
+		}
+	}
+
+	public Puzzle getRacine() {
+		return racine;
+	}
+
+	public void setRacine(Puzzle racine) {
+		this.racine = racine;
+	}
+
+	public TreeSet<Puzzle> getFrontiere() {
+		return frontiere;
+	}
+
+	public void setFrontiere(TreeSet<Puzzle> frontiere) {
+		this.frontiere = frontiere;
+	}
+
+	public Set<Puzzle> getExplored() {
+		return explored;
+	}
+
+	public void setExplored(Set<Puzzle> explored) {
+		this.explored = explored;
+	}
+
+	public Puzzle getEnd() {
+		return end;
+	}
+
+	public void setEnd(Puzzle end) {
+		this.end = end;
+	}
+	
+	
+
+	public boolean ucs() {
+		if (this.racine.isSuccess())
+			return true;
+		this.frontiere.add(this.end);
+		while (true) {
+			if (this.frontiere.isEmpty())
+				return false;
+			Puzzle node = this.frontiere.pollFirst();
+			this.explored.add(node);
+			for (String b : ACTIONS) {
+				Puzzle child = new Puzzle(node);
+				if(b.equals("H")){
+					if(node.moveHaut()) {
+						child.setScore(this.Score(child, "H"));
+						child.Haut();
+					}
+				}
+				if(b.equals("B")){
+					if(node.moveBas()) {
+						child.setScore(this.Score(child, "B"));
+						child.Bas();
+					}
+				}
+				if(b.equals("D")){
+					if(node.moveDroite()) {
+						child.setScore(this.Score(child, "D"));
+						child.Droite();
+					}
+				}
+				if(b.equals("G")){
+					if(node.moveGauche()) {
+						child.setScore(this.Score(child, "G"));
+						child.Gauche();
+					}
+				}
+				if(!this.explored.contains(child) && !this.frontiere.contains(child)) {
+					if(child.isSuccess()) {
+						this.end = child;
+						return true;
+					}
+					this.frontiere.add(child);
+				}
+				Puzzle p = new Puzzle();
+				if(this.frontiere.contains(child)) {
+					TreeSet<Puzzle> tmp = (TreeSet<Puzzle>) frontiere.clone();
+					Iterator<Puzzle> it = tmp.iterator();
+					while(it.hasNext()) {
+						p = it.next();
+						if(p.getTab().equals(child.getTab())) {
+							break;
+						}
+					}
+					if(p.getScore()>child.getScore()) {
+						this.frontiere.remove(p);
+						this.frontiere.add(child);
+					}
+				}
+			}
+		}
+	}
+
+}
