8917d131bb462b96e55f2d4f14153525dcf09694
FloryanKieffer <floryan@mbp-de-floryan.home>
2021-06-16 19:04:28 +0200

    Exp

---
+++ Iapro/src/Experimentation.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Experimentation {
+	
+	private ArrayList<Puzzle> ListPuzzle;
+	private int tailleList;
+	private int taillePuzzle;
+	
+	public Experimentation(int tailleList, int taillePuzzle) {
+		this.tailleList = tailleList;
+		this.taillePuzzle = taillePuzzle;
+		this.ListPuzzle = new ArrayList<Puzzle>();
+	}
+	
+	public void init() {
+		for(int i =0; i <this.tailleList;i++) {
+			Puzzle p = new Puzzle(this.taillePuzzle);
+			this.ListPuzzle.add(p);
+		}
+	}
+
+}
